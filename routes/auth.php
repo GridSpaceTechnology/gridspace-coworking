459e9aee90574aca80b4ad0ac83898cc377eec8f
charlesmfb <mrcharlesekpo37@gmail.com>
2026-03-12 19:27:51 +0100

    Optimize localhost performance - fix route conflicts and caching

--- routes/auth.php
+++ routes/auth.php
@@ -50,10 +50,7 @@ Route::middleware('auth')->group(function () {
         ->name('verification.send');
 
     Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
-        ->name('password.confirm');
-
-    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store'])
-        ->name('password.confirm');
+        ->name('confirm-password');
 
     Route::put('password', [PasswordController::class, 'update'])->name('password.update');
 
